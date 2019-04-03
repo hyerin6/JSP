c30b08e227412f7d2f63a7b2b457ceb1fcf95e5a
hyerin6 <hyerinn6@gmail.com>
2019-04-03 11:18:17 +0900

    4/3

--- jdbc1/src/lecture1/jdbc1/StudentDAO.java
+++ jdbc1/src/lecture1/jdbc1/StudentDAO.java
@@ -12,7 +12,9 @@ public class StudentDAO {
 	public static List<Student> findAll() throws Exception {
 		String sql = "SELECT s.*, d.departmentName " +
 				"FROM student s LEFT JOIN department d ON s.departmentId = d.id";
-		try (Connection connection = DB.getConnection("student1");
+		// try with resource : 마지막에 세미콜론(;) 있으면 안됨.
+		// Connection 도 꼭 close 해줘야함!
+		try (Connection connection = DB.getConnection("student1"); // DB 이름 주의
 				PreparedStatement statement = connection.prepareStatement(sql);
 				ResultSet resultSet = statement.executeQuery()) {
 			ArrayList<Student> list = new ArrayList<Student>();
